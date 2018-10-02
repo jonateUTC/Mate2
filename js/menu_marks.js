6fbb42197334881e13d523c032080c6c578aa05f
jonateUTC <alexjohna195@gmail.com>
2018-10-02 09:12:33 -0500

    js

---
+++ js/menu_marks.js
@@ -0,0 +1,21 @@
+var mrk = [
+    {
+        name: 'ACTIVIDADES',
+        folder: 'actividades',
+        items: [
+            {name:'UNIDAD 1',items:[{name:'Prueba diagnóstica',link:'1',page:'13'},{name:'Conjuntos',link:'2',page:'15'},{name:'Secuencias y patrones',link:'3',page:'17'},{name:'Secuencias y patrones',link:'4',page:'17'},{name:'Secuencias y patrones',link:'5',page:'17'},{name:'Secuencias y patrones',link:'6',page:'17'},{name:'Números de 1 al 5 y el 0',link:'7',page:'19'},{name:'Números de 1 al 5 y el 1',link:'8',page:'19'},{name:'Números del 6 al 10',link:'9',page:'20'},{name:'Números del 6 al 9',link:'10',page:'21'},{name:'Números del 6 al 10',link:'11',page:'21'},{name:'Números dígitos',link:'12',page:'22'},{name:'Números dígitos',link:'13',page:'23'},{name:'Números dígitos',link:'14',page:'23'},{name:'Números dígitos',link:'15',page:'23'},{name:'Los números dígitos en la semirecta numérica',link:'16',page:'25'},{name:'Los números dígitos en la semirecta numérica',link:'17',page:'25'},{name:'Secuencias y series numéricas ascendentes y descendentes',link:'18',page:'27'},{name:'Secuencias y series numéricas ascendentes y descendentes',link:'19',page:'27'},{name:'Relación de orden > mayor que, < menor que, = igual a',link:'20',page:'29'},{name:'Relación de orden > mayor que, < menor que, = igual a',link:'21',page:'29'},{name:'Relación de orden > mayor que, < menor que, = igual a',link:'22',page:'29'},{name:'Refuerzo mis destrezas',link:'23',page:'30'},{name:'Refuerzo mis destrezas',link:'24',page:'31'},{name:'Refuerzo mis destrezas',link:'25',page:'31'},{name:'Refuerzo mis destrezas',link:'26',page:'31'},{name:'Cuerpos geométricos (prismas, pirámides, cilindros y conos)',link:'27',page:'35'},{name:'Formas cuadradas, rectangulares, triangulares y circulares',link:'28',page:'37'},{name:'Formas cuadradas, rectangulares, triangulares y circulares',link:'29',page:'37'},{name:'Antes y después',link:'30',page:'39'},{name:'Antes y después',link:'31',page:'39'},{name:'Refuerzo mis destrezas',link:'32',page:'40'},{name:'Refuerzo mis destrezas',link:'33',page:'41'},{name:'Recolección de datos',link:'34',page:'43'},{name:'Recolección de datos',link:'35',page:'43'},{name:'Refuerzo mis destrezas',link:'36',page:'44'},{name:'Me preparo para mi evaluación',link:'37',page:'46'},{name:'Evaluación sumativa',link:'38',page:'47'},]},
+        
+
+        ]
+    },
+    {
+        name: 'ANIMACIONES',
+        folder: 'animaciones',
+        items: [
+            {name:'UNIDAD 1',items:[{name:'Números del 1 al 5 y el 0',link:'1',page:'18'},{name:'Números del 1 al 5 y el 0',link:'2',page:'18'},{name:'Números del 1 al 5 y el 0',link:'3',page:'19'},{name:'Números del 1 al 5 y el 0',link:'4',page:'20'},{name:'Números del 1 al 5 y el 0',link:'5',page:'21'},{name:'Números del 1 al 5 y el 0',link:'6',page:'22'},{name:'Números dígitos',link:'7',page:'24'},{name:'Los números dígitos en la semirrecta numérica',link:'8',page:'28'},{name:'Relación de orden > mayor que, < menor que, = igual a',link:'9',page:'34'},{name:'Cuerpos geométricos (prismas, pirámides, cilindros y conos)',link:'10',page:'38'},{name:'Antes y después',link:'11',page:'42'},]},
+
+        
+        ]
+    }
+]
+
